7404ab5c079554fde2c6b1b12df24e548e5258fb
sunyanfei <meetsyf@gmail.com>
2019-12-05 23:54:31 +0800

    Site updated: 2019-12-05 23:54:28

--- js/utils.js
+++ js/utils.js
@@ -1,13 +1,5 @@
 /* global NexT, CONFIG */
 
-HTMLElement.prototype.outerHeight = function(flag) {
-  var height = this.offsetHeight;
-  if (!flag) return height;
-  var style = window.getComputedStyle(this);
-  height += parseInt(style.marginTop, 10) + parseInt(style.marginBottom, 10);
-  return height;
-};
-
 HTMLElement.prototype.wrap = function(wrapper) {
   this.parentNode.insertBefore(wrapper, this);
   this.parentNode.removeChild(this);
@@ -20,7 +12,7 @@ NexT.utils = {
    * Wrap images with fancybox.
    */
   wrapImageWithFancyBox: function() {
-    document.querySelectorAll('.post-body :not(a) > img').forEach(element => {
+    document.querySelectorAll('.post-body :not(a) > img, .post-body > img').forEach(element => {
       var $image = $(element);
       var imageLink = $image.attr('data-src') || $image.attr('src');
       var $imageWrapLink = $image.wrap(`<a class="fancybox fancybox.image" href="${imageLink}" itemscope itemtype="http://schema.org/ImageObject" itemprop="url"></a>`).parent('a');
@@ -67,27 +59,19 @@ NexT.utils = {
    * One-click copy code support.
    */
   registerCopyCode: function() {
-    document.querySelectorAll('figure.highlight').forEach(e => {
-      const initButton = button => {
-        if (CONFIG.copycode.style === 'mac') {
-          button.innerHTML = '<i class="fa fa-clipboard"></i>';
-        } else {
-          button.innerText = CONFIG.translation.copy_button;
-        }
-      };
+    document.querySelectorAll('figure.highlight').forEach(element => {
       const box = document.createElement('div');
-      box.classList.add('highlight-wrap');
-      e.wrap(box);
-      e.parentNode.insertAdjacentHTML('beforeend', '<div class="copy-btn"></div>');
-      var button = e.parentNode.querySelector('.copy-btn');
+      element.wrap(box);
+      box.classList.add('highlight-container');
+      box.insertAdjacentHTML('beforeend', '<div class="copy-btn"><i class="fa fa-clipboard"></i></div>');
+      var button = element.parentNode.querySelector('.copy-btn');
       button.addEventListener('click', event => {
         var target = event.currentTarget;
-        var code = [...target.parentNode.querySelectorAll('.code .line')].map(element => {
-          return element.innerText;
+        var code = [...target.parentNode.querySelectorAll('.code .line')].map(line => {
+          return line.innerText;
         }).join('\n');
         var ta = document.createElement('textarea');
-        var yPosition = window.scrollY;
-        ta.style.top = yPosition + 'px'; // Prevent page scrolling
+        ta.style.top = window.scrollY + 'px'; // Prevent page scrolling
         ta.style.position = 'absolute';
         ta.style.opacity = '0';
         ta.readOnly = true;
@@ -100,7 +84,7 @@ NexT.utils = {
         ta.readOnly = false;
         var result = document.execCommand('copy');
         if (CONFIG.copycode.show_result) {
-          target.innerText = result ? CONFIG.translation.copy_success : CONFIG.translation.copy_failure;
+          target.querySelector('i').className = result ? 'fa fa-check' : 'fa fa-times';
         }
         ta.blur(); // For iOS
         target.blur();
@@ -112,36 +96,35 @@ NexT.utils = {
       });
       button.addEventListener('mouseleave', event => {
         setTimeout(() => {
-          initButton(event.target);
+          event.target.querySelector('i').className = 'fa fa-clipboard';
         }, 300);
       });
-      initButton(button);
     });
   },
 
   wrapTableWithBox: function() {
-    document.querySelectorAll('table').forEach(table => {
+    document.querySelectorAll('table').forEach(element => {
       const box = document.createElement('div');
       box.className = 'table-container';
-      table.wrap(box);
+      element.wrap(box);
     });
   },
 
   registerVideoIframe: function() {
     document.querySelectorAll('iframe').forEach(element => {
-      const SUPPORTED_PLAYERS = [
+      const supported = [
         'www.youtube.com',
         'player.vimeo.com',
         'player.youku.com',
         'player.bilibili.com',
         'www.tudou.com'
-      ];
-      const pattern = new RegExp(SUPPORTED_PLAYERS.join('|'));
-      if (!element.parentNode.matches('.video-container') && element.src.search(pattern) > 0) {
+      ].some(host => element.src.includes(host));
+      if (supported && !element.parentNode.matches('.video-container')) {
         const box = document.createElement('div');
         box.className = 'video-container';
         element.wrap(box);
-        let width = Number(element.width); let height = Number(element.height);
+        let width = Number(element.width);
+        let height = Number(element.height);
         if (width && height) {
           element.parentNode.style.paddingTop = (height / width * 100) + '%';
         }
@@ -164,7 +147,7 @@ NexT.utils = {
         scrollPercent = Math.min(scrollPercentRounded, 100) + '%';
       }
       if (backToTop) {
-        window.scrollY > THRESHOLD ? backToTop.classList.add('back-to-top-on') : backToTop.classList.remove('back-to-top-on');
+        backToTop.classList.toggle('back-to-top-on', window.scrollY > THRESHOLD);
         backToTop.querySelector('span').innerText = scrollPercent;
       }
       if (readingProgressBar) {
@@ -174,7 +157,7 @@ NexT.utils = {
 
     backToTop && backToTop.addEventListener('click', () => {
       window.anime({
-        targets  : [document.documentElement, document.body],
+        targets  : document.scrollingElement,
         duration : 500,
         easing   : 'linear',
         scrollTop: 0
@@ -187,20 +170,20 @@ NexT.utils = {
    */
   registerTabsTag: function() {
     // Binding `nav-tabs` & `tab-content` by real time permalink changing.
-    document.querySelectorAll('.tabs ul.nav-tabs .tab').forEach(tab => {
-      tab.addEventListener('click', event => {
+    document.querySelectorAll('.tabs ul.nav-tabs .tab').forEach(element => {
+      element.addEventListener('click', event => {
         event.preventDefault();
         var target = event.currentTarget;
         // Prevent selected tab to select again.
         if (!target.classList.contains('active')) {
           // Add & Remove active class on `nav-tabs` & `tab-content`.
-          [...target.parentNode.children].forEach(item => {
-            item.classList.remove('active');
+          [...target.parentNode.children].forEach(element => {
+            element.classList.remove('active');
           });
           target.classList.add('active');
           var tActive = document.getElementById(target.querySelector('a').getAttribute('href').replace('#', ''));
-          [...tActive.parentNode.children].forEach(item => {
-            item.classList.remove('active');
+          [...tActive.parentNode.children].forEach(element => {
+            element.classList.remove('active');
           });
           tActive.classList.add('active');
           // Trigger event
@@ -216,8 +199,8 @@ NexT.utils = {
 
   registerCanIUseTag: function() {
     // Get responsive height passed from iframe.
-    window.addEventListener('message', e => {
-      var data = e.data;
+    window.addEventListener('message', event => {
+      var data = event.data;
       if ((typeof data === 'string') && (data.indexOf('ciu_embed') > -1)) {
         var featureID = data.split(':')[1];
         var height = data.split(':')[2];
@@ -232,11 +215,7 @@ NexT.utils = {
       if (!target) return;
       var isSamePath = target.pathname === location.pathname || target.pathname === location.pathname.replace('index.html', '');
       var isSubPath = target.pathname !== CONFIG.root && location.pathname.indexOf(target.pathname) === 0;
-      if (target.hostname === location.hostname && (isSamePath || isSubPath)) {
-        element.classList.add('menu-item-active');
-      } else {
-        element.classList.remove('menu-item-active');
-      }
+      element.classList.toggle('menu-item-active', target.hostname === location.hostname && (isSamePath || isSubPath));
     });
   },
 
@@ -250,7 +229,7 @@ NexT.utils = {
         var target = document.getElementById(event.currentTarget.getAttribute('href').replace('#', ''));
         var offset = target.getBoundingClientRect().top + window.scrollY;
         window.anime({
-          targets  : [document.documentElement, document.body],
+          targets  : document.scrollingElement,
           duration : 500,
           easing   : 'linear',
           scrollTop: offset + 10
@@ -288,7 +267,7 @@ NexT.utils = {
         index = sections.indexOf(entry.target);
         return index === 0 ? 0 : index - 1;
       }
-      for (;index < entries.length; index++) {
+      for (; index < entries.length; index++) {
         if (entries[index].boundingClientRect.top <= 0) {
           entry = entries[index];
         } else {
@@ -313,7 +292,9 @@ NexT.utils = {
         rootMargin: marginTop + 'px 0px -100% 0px',
         threshold : 0
       });
-      sections.forEach(item => intersectionObserver.observe(item));
+      sections.forEach(element => {
+        element && intersectionObserver.observe(element);
+      });
     }
     createIntersectionObserver(document.documentElement.scrollHeight);
   },
@@ -321,7 +302,6 @@ NexT.utils = {
   hasMobileUA: function() {
     var ua = navigator.userAgent;
     var pa = /iPad|iPhone|Android|Opera Mini|BlackBerry|webOS|UCWEB|Blazer|PSP|IEMobile|Symbian/g;
-
     return pa.test(ua);
   },
 
@@ -359,10 +339,10 @@ NexT.utils = {
    */
   initSidebarDimension: function() {
     var sidebarNav = document.querySelector('.sidebar-nav');
-    var sidebarNavHeight = sidebarNav.style.display !== 'none' ? sidebarNav.outerHeight(true) : 0;
+    var sidebarNavHeight = sidebarNav.style.display !== 'none' ? sidebarNav.offsetHeight : 0;
     var sidebarOffset = CONFIG.sidebar.offset || 12;
     var sidebarb2tHeight = CONFIG.back2top.enable && CONFIG.back2top.sidebar ? document.querySelector('.back-to-top').offsetHeight : 0;
-    var sidebarSchemePadding = CONFIG.sidebarPadding + sidebarNavHeight + sidebarb2tHeight;
+    var sidebarSchemePadding = (CONFIG.sidebar.padding * 2) + sidebarNavHeight + sidebarb2tHeight;
     // Margin of sidebar b2t: 8px -10px -20px, brings a different of 12px.
     if (NexT.utils.isPisces() || NexT.utils.isGemini()) sidebarSchemePadding += (sidebarOffset * 2) - 12;
     // Initialize Sidebar & TOC Height.
